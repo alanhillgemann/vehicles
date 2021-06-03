644df592ac3b2af26d01e1c5563870726a86993f
“Alan <alan.hillgemann@anz.com>
2021-06-03 13:16:27 +1000

    feat: implement pricing-service tests

---
+++ pricing-service/src/test/java/com/udacity/pricing/web/PricingControllerIntegrationTests.java
@@ -0,0 +1,31 @@
+package com.udacity.pricing.web;
+
+import com.udacity.pricing.domain.price.Price;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class PricingControllerIntegrationTests {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void getLocation() {
+        ResponseEntity<Price> response =
+                this.restTemplate.getForEntity("http://localhost:" + port + "/services/price?vehicleId=1", Price.class);
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+    }
+}
